78720548fa1ed901ca7373a32d56e7fc526a755d
TodRieter <thomas@wilson.land>
2021-11-08 12:03:19 -0600

    update branch

---
+++ flexible testing/flextest/build/app/generated/source/buildConfig/debug/com/example/flextest/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.flextest;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.flextest";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
